7d6e382722530071b7588762dd21a9fa16322590
MEURANT <alexandremeurant@hotmail.fr>
2017-09-16 20:10:48 +0200

    Added some instructions to display selected color

--- js/lite_brite.js
+++ js/lite_brite.js
@@ -4,14 +4,19 @@ function main() {
         // gets the element's class:
         let selectedColor = $(this).attr('class');
         // checks the selected color block and gives color to boxes:
-        if (selectedColor === "select-color cyan not-selected"){
+        if (selectedColor === "select-color cyan not-selected") {
+            colorClass = "cyan";
             $('.box').css('color', '#1ebacc');
-        } else if (selectedColor === "select-color yellow not-selected"){
+        } else if (selectedColor === "select-color yellow not-selected") {
+            colorClass = "yellow";
             $('.box').css('color', '#ffea4e');
-        } else if (selectedColor === "select-color magenta not-selected"){
+        } else if (selectedColor === "select-color magenta not-selected") {
             $('.box').css('color', '#ff2f9e');
         }
-    }
+        // Displays selected color:
+        $(this).removeClass("not-selected");
+        $(this).siblings().addClass('not-selected');
+    });
 }
 
 $(document).ready(main);
