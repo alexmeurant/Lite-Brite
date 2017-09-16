816fe75e7816607c04b2b8b82193b0c2d757b38f
MEURANT <alexandremeurant@hotmail.fr>
2017-09-16 20:19:12 +0200

    Added function to change color of boxes

--- js/lite_brite.js
+++ js/lite_brite.js
@@ -3,6 +3,9 @@ function main() {
     $('.select-color').on('click', function () {
         // gets the element's class:
         let selectedColor = $(this).attr('class');
+        // holds the color in a variable:
+        let colorClass = '';
+        
         // checks the selected color block and gives color to boxes:
         if (selectedColor === "select-color cyan not-selected") {
             colorClass = "cyan";
@@ -11,11 +14,17 @@ function main() {
             colorClass = "yellow";
             $('.box').css('color', '#ffea4e');
         } else if (selectedColor === "select-color magenta not-selected") {
+            colorClass = "magenta";
             $('.box').css('color', '#ff2f9e');
         }
         // Displays selected color:
         $(this).removeClass("not-selected");
         $(this).siblings().addClass('not-selected');
+    });
+    
+    // selects boxes and changes their color:   
+    $('.box').on('click', function () {
+        $(this).toggleClass('colorClass');
     });
 }
 
